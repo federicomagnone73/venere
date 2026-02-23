cdcd41c3c9f7aaa094d30464cbdddfb9b8124bfb
Federico Magnone <59465989+federicomagnone73@users.noreply.github.com>
2026-02-23 23:54:49 +0100

    Update person.js

--- app/routes/person.js
+++ app/routes/person.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const personaController = require('../controllers/personaController');
+const personController = require('../controllers/personController');
 const authMiddleware = require('../middleware/auth'); // Importa il middleware
 
 // Applica il middleware a tutte le rotte di questo file
 router.use(authMiddleware);
 
 // Ora queste rotte sono protette
-router.get('/', personaController.getAll);
-router.post('/', personaController.create);
+router.get('/', personController.getAll);
+router.post('/', personController.create);
 
 module.exports = router;
