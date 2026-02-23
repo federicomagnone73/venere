63b15be273cf78540c397a0a759cee0826922665
Federico Magnone <59465989+federicomagnone73@users.noreply.github.com>
2026-02-23 23:15:36 +0100

    Update personController.js

--- app/controllers/personController.js
+++ app/controllers/personController.js
@@ -1,1 +1,13 @@
+// Qui andrebbero le query a MongoDB (es. usando un modello Mongoose)
+exports.getAll = async (req, res) => {
+    try {
+        res.status(200).json({ message: "Lista persone recuperata" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
 
+exports.create = async (req, res) => {
+    const { nome, cognome } = req.body;
+    res.status(201).json({ message: "Persona creata", data: { nome, cognome } });
+};
