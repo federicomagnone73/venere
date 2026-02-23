faea4ee1b8ed8004e331af32a12ced58be8ae593
Federico Magnone <59465989+federicomagnone73@users.noreply.github.com>
2026-02-23 23:13:42 +0100

    Update person.js

--- app/routes/person.js
+++ app/routes/person.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const personaController = require('../controllers/personaController');
 
+router.get('/', personaController.getAll);
+router.post('/', personaController.create);
+
+module.exports = router;
